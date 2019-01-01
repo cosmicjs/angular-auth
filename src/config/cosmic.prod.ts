d01deb537901846ed0034452d18be1127bba71c1
a9kitkumar <a9kitsharma@gmail.com>
2019-01-02 02:09:19 +0530

    app.json changes

--- src/config/cosmic.prod.ts
+++ src/config/cosmic.prod.ts
@@ -1,9 +1,5 @@
 export const config = {
     production: true,
-    read_key: 'h1uZgF25TZZJYrdLS6z1GMOTGH3qPstng0ylNztC1hAQCVH0IV',
     write_key: 'CY4ZYtsD6OS4ydSbXL3KHMiA1t96VE8o0s18A5sMgkcwJd9XAE',
-    bucket_id: '5c251b29ce27621084a397fe',
-    bucket_slug: 'angularauth', 
-    bucket_name: 'angularauth', 
-    URL: 'https://api.cosmicjs.com/v1/',
+    bucket_name: 'angularauth',     
     };
