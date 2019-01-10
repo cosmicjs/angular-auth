02a1c05f7d36a1f140bcd590dd0a0b9458817c02
a9kitkumar <46286692+a9kitkumar@users.noreply.github.com>
2019-01-10 23:41:23 +0530

    Update cosmic.config.ts

--- src/config/cosmic.config.ts
+++ src/config/cosmic.config.ts
@@ -3,6 +3,6 @@
         production: false,
         read_key: '',
         write_key: '',
-        bucket_slug: 'auth-test',
+        bucket_slug: 'angularauthentication',
         URL: 'https://api.cosmicjs.com/v1/',
     };
